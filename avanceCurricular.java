98386587bc28df65e99b66bb866f0a7a8670f551
Joaquin Fuenzalida <100775665+JoakoFuenzalida@users.noreply.github.com>
2023-08-31 03:33:47 -0400

    Agregue un menu donde se puede agregar la asigntura y con él, su nombre y creditos,  y tambien implemente el metodo obtenerAlumnoPorNombre

--- avanceCurricular.java
+++ avanceCurricular.java
@@ -12,7 +12,7 @@ class Alumno {
     public void agregarAsignaturas(Asignatura nuevaAsignatura) {
         Asignatura[] nuevasAsignaturas = new Asignatura[asignaturas.length + 1];
 
-        for(int i = 0 ; i < asignaturas.length ; i++) {
+        for (int i = 0; i < asignaturas.length; i++) {
             nuevasAsignaturas[i] = asignaturas[i];
         }
 
@@ -27,13 +27,13 @@ class Asignatura {
     private String nombreProfesor;
     private int creditos;
     private boolean completada;
-    
+
     public Asignatura(String nombreAsignatura, String nombreProfesor, int creditos) {
         this.nombreAsignatura = nombreAsignatura;
         this.nombreProfesor = nombreProfesor;
         this.creditos = creditos;
     }
-    
+
     public void marcarComoCompletada() {
         completada = true;
     }
@@ -44,7 +44,7 @@ class Asignatura {
 
 }
 
-class Sistema{
+class Sistema {
     private ArrayList<Alumno> alumnos;
     private HashMap<String, Alumno> mapaAlumnosNombre;
 
@@ -58,29 +58,64 @@ class Sistema{
         mapaAlumnosNombre.put(nuevoAlumno.nombreAlumno, nuevoAlumno);
     }
 
-    public void eliminarAlumno(Alumno alumnoParaEliminar)
-    {
+    public void eliminarAlumno(Alumno alumnoParaEliminar) {
         alumnos.remove(alumnoParaEliminar);
         mapaAlumnosNombre.remove(alumnoParaEliminar.nombreAlumno, alumnoParaEliminar);
     }
 
-    public void listarAlumnosNombre(){
+    public void listarAlumnosNombre() {
         Set<String> nombres = mapaAlumnosNombre.keySet();
 
-        for(String nombre : nombres)
-        {
+        for (String nombre : nombres) {
             System.out.println(nombre);
         }
     }
 
+    public Alumno obtenerAlumnoPorNombre(String nombre) {
+        return mapaAlumnosNombre.get(nombre);
+    }
+
 }
 
-
-
-
-public class avanceCurricular{
+public class avanceCurricular {
     public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Agregar asignatura a alumno");
+            System.out.println("2. Salir");
+            System.out.print("Selecciona una opcion: ");
+            int opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                scanner.nextLine();
+                System.out.print("Ingrese el nombre del alumno: ");
+                String nombreAlumno = scanner.nextLine();
+
+                Alumno alumno = sistema.obtenerAlumnoPorNombre(nombreAlumno);
+                if (alumno != null) {
+                    System.out.print("Ingrese el nombre de la asignatura: ");
+                    String nombreAsignatura = scanner.nextLine();
+                    System.out.print("Ingrese el nombre del profesor: ");
+                    String nombreProfesor = scanner.nextLine();
+                    System.out.print("Ingrese la cantidad de creditos: ");
+                    int creditos = scanner.nextInt();
+
+                    Asignatura nuevaAsignatura = new Asignatura(nombreAsignatura, nombreProfesor, creditos);
+                    alumno.agregarAsignaturas(nuevaAsignatura);
+                    System.out.println("Asignatura agregada con exito.");
+                } else {
+                    System.out.println("Alumno no encontrado.");
+                }
+            } else if (opcion == 2) {
+                System.out.println("Saliendo del programa.");
+                break;
+            } else {
+                System.out.println("Opcion inválida. Por favor, elija una opción valida.");
+            }
+        }
 
     }
 }
