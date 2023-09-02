95812d4a4251d9dde43c7107dcd886c40bb14c19
Joaquin Fuenzalida <100775665+JoakoFuenzalida@users.noreply.github.com>
2023-09-01 20:48:16 -0400

    Elimine parte del menu y funcionalidades.
    Lo hice para solo dejar lo importante por ahora y ademas que agregue alumnos de prueba

--- avanceCurricular.java
+++ avanceCurricular.java
@@ -13,7 +13,7 @@ class Alumno {
         this.asignaturas = new ArrayList<>();
     }
 
-    public int getRut(){
+    public int getRut() {
         return rut;
     }
 
@@ -43,7 +43,7 @@ class Menu {
             System.out.println("0. Salir");
             System.out.print("Selecciona una opcion: ");
             opcion = scanner.nextInt();
-            switch(opcion){
+            switch (opcion) {
                 case 1:
                     scanner.nextLine();
                     System.out.print("Ingrese el nombre de la asignatura: ");
@@ -57,14 +57,16 @@ class Menu {
                     System.out.println("Asignatura agregada con exito.");
                     break;
                 case 0:
-                    System.out.println("Volviendo al menu principal."); 
+                    System.out.println("Volviendo al menu principal.");
                     break;
                 default:
                     System.out.println("Opcion inválida. Por favor, elija una opcion valida.");
                     break;
             }
         }
+        scanner.close();
     }
+
 }
 
 class Asignatura {
@@ -120,13 +122,6 @@ class Sistema {
         }
     }
 
-    public void listarAlumnosRut() {
-        Set<Integer> ruts = mapaAlumnosRut.keySet();
-
-        for(int rut : ruts) {
-            System.out.println(rut);
-        }
-    }
 
     public Alumno obtenerAlumno(String nombre) {
         return mapaAlumnosNombre.get(nombre);
@@ -141,9 +136,50 @@ class Sistema {
 public class avanceCurricular {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
+
+        Scanner scanner = new Scanner(System.in);
+
         Alumno alumno1 = new Alumno("Juan", 12345678);
         sistema.agregarAlumno(alumno1);
-        Menu menuAlumno1 = new Menu(alumno1);
-        menuAlumno1.mostrarMenuAlumno();
+        Alumno alumno2 = new Alumno("Pedro", 87654321);
+        sistema.agregarAlumno(alumno2);
+        Alumno alumno3 = new Alumno("Maria", 13579246);
+        sistema.agregarAlumno(alumno3);
+        Alumno alumno4 = new Alumno("Jose", 24681357);
+        sistema.agregarAlumno(alumno4);
+
+        System.out.println("Ingrese el nombre o rut (sin puntos ni guion) del alumno que desea buscar: ");
+        String nombreO_RutAlumnoMostrar = scanner.nextLine();
+
+        Alumno alumnoMostrar = null;
+
+        
+        try {
+            int rut = Integer.parseInt(nombreO_RutAlumnoMostrar);
+            alumnoMostrar = sistema.obtenerAlumno(rut);
+        } catch (NumberFormatException e) {
+            
+            alumnoMostrar = sistema.obtenerAlumno(nombreO_RutAlumnoMostrar);
+        }
+
+        while (alumnoMostrar == null) {
+            System.out.println(
+                    "No se encontro el alumno. Ingrese nuevamente el nombre o rut del alumno que desea buscar: ");
+            nombreO_RutAlumnoMostrar = scanner.nextLine();
+            
+            try {
+                int rut = Integer.parseInt(nombreO_RutAlumnoMostrar);
+                alumnoMostrar = sistema.obtenerAlumno(rut);
+            } catch (NumberFormatException e) {
+                
+                alumnoMostrar = sistema.obtenerAlumno(nombreO_RutAlumnoMostrar);
+            }
+        }
+
+        Menu menuAlumnoMostrar = new Menu(alumnoMostrar);
+        menuAlumnoMostrar.mostrarMenuAlumno();
+
+        scanner.close();
     }
+
 }
