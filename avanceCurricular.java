08b6dace4e4adb2ef648d71a6e374c7241044238
Joaquin Fuenzalida <100775665+JoakoFuenzalida@users.noreply.github.com>
2023-09-29 21:08:33 -0300

    Agregue la excepcion al momento de no encontrar el alumno

--- avanceCurricular.java
+++ avanceCurricular.java
@@ -1,12 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package com.mycompany.avancecurricular;
 
 import java.util.*;
 
-public class avanceCurricular {
+public class main {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
 
@@ -24,53 +25,50 @@ public class avanceCurricular {
         int opcionPrincipal = 1;
 
         while (opcionPrincipal != 0) {
-            System.out.println("Menu Principal:");
-            System.out.println("1. Ingresar a Avance Curricular");
-            System.out.println("0. Salir");
-            System.out.print("Selecciona una opcion: ");
-            opcionPrincipal = scanner.nextInt();
+        System.out.println("Menu Principal:");
+        System.out.println("1. Ingresar a Avance Curricular");
+        System.out.println("0. Salir");
+        System.out.print("Selecciona una opcion: ");
+        opcionPrincipal = scanner.nextInt();
 
-            switch (opcionPrincipal) {
-                case 1:
-                    scanner.nextLine(); // Consumir la nueva línea pendiente
-                    System.out.println("Ingrese el nombre o rut (sin puntos ni guion) del alumno que desea buscar: ");
-                    String nombreO_RutAlumnoMostrar = scanner.nextLine();
+        switch (opcionPrincipal) {
+            case 1:
+                scanner.nextLine();
+                System.out.println("Ingrese el nombre o rut (sin puntos ni guion) del alumno que desea buscar: ");
+                String nombreO_RutAlumnoMostrar = scanner.nextLine();
 
-                    Alumno alumnoMostrar = null;
-
-                    try {
+                
+                Alumno alumnoMostrar = null;
+                
+                try {
                         int rut = Integer.parseInt(nombreO_RutAlumnoMostrar);
                         alumnoMostrar = sistema.obtenerAlumno(rut);
                     } catch (NumberFormatException e) {
                         alumnoMostrar = sistema.obtenerAlumno(nombreO_RutAlumnoMostrar);
                     }
 
-                    while (alumnoMostrar == null) {
-                        System.out.println(
-                                "No se encontro el alumno. Ingrese nuevamente el nombre o rut del alumno que desea buscar: ");
-                        nombreO_RutAlumnoMostrar = scanner.nextLine();
-
-                        try {
-                            int rut = Integer.parseInt(nombreO_RutAlumnoMostrar);
-                            alumnoMostrar = sistema.obtenerAlumno(rut);
-                        } catch (NumberFormatException e) {
-                            alumnoMostrar = sistema.obtenerAlumno(nombreO_RutAlumnoMostrar);
-                        }
+                if (alumnoMostrar == null) {
+                    
+                    try {
+                        throw new Excepciones.AlumnoNoEncontradoException("El alumno '" + nombreO_RutAlumnoMostrar + "' no se encuentra en el sistema.");
+                    } catch (Excepciones.AlumnoNoEncontradoException e) {
+                        System.out.println("Error: " + e.getMessage());
                     }
-
+                } else {
                     Menu menuAlumnoMostrar = new Menu(alumnoMostrar);
                     menuAlumnoMostrar.mostrarMenuAlumno();
-                    break;
-                case 0:
-                    System.out.println("Saliendo del programa.");
-                    break;
-                default:
-                    System.out.println("Opción inválida. Por favor, elija una opción válida.");
-                    break;
-            }
+                }
+                break;
+            case 0:
+                System.out.println("Saliendo del programa...");
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, elija una opción válida.");
+                break;
         }
+    }
 
-        scanner.close();
+    scanner.close();
     }
 
 }
